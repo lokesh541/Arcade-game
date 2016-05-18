918e7106d7f82f259ad4f59635e5d655ff4f946d
lokesh541 <lrbeeram@gmail.com>
2016-05-18 10:09:45 +0530

    comments added

--- js/engine.js
+++ js/engine.js
@@ -109,11 +109,12 @@ var Engine = (function(global) {
          */
         var rowImages = [
                 'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/stone-block.png',// Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png',   // Row 3 of 3 of stone-block                          
                 'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/grass-block.png',
+                   // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -167,8 +168,8 @@ var Engine = (function(global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        'images/stone-block.png',
         'images/water-block.png',
+        'images/stone-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png'
